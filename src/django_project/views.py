a420be8e02d4a0f76b76c81861a7aad3ac65724c
Abd017 <abdulwahab4534@gmail.com>
2019-12-11 14:51:59 +0530

    My Blog app, first commit

---
+++ src/django_project/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .forms import ContactForm
+from blog.models import BlogPost
+
+
+def home_page(request):
+    my_title = "Hello there...."
+    qs = BlogPost.objects.all()[:5]
+    context = {"title": "Welcome to My Blog...", 'blog_list': qs}
+    return render(request, "home.html", context)
+
+
+def contact_page(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {
+        'title': "Contact Us",
+        "form": form
+    }
+    return render(request, 'form.html', context)
